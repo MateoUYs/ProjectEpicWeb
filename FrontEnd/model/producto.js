a4a4f932bcd5d9f524bf4dd0a16044f59eff2ff8
Batistiano <batistaangel2007@gmail.com>
2024-08-29 14:12:09 -0300

    Creacion de los model

--- FrontEnd/model/producto.js
+++ FrontEnd/model/producto.js
@@ -11,7 +11,12 @@ export default class Producto{
     constructor(precio,nombre,imagen,talle,descripcion,color,stock,id){
         this.precio=precio;
         this.nombre=nombre;
-    
+        this.imagen=imagen;
+        this.talle=talle;
+        this.descripcion=descripcion;
+        this.color=color;
+        this.stock=stock;
+        this.id=id;
     }
 
 }
