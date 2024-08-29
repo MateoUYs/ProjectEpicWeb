aa140fd4217974e062e1db62e9d2641a3c4c6b39
Batistiano <batistaangel2007@gmail.com>
2024-08-29 15:04:37 -0300

    SESIOn

---
+++ FrontEnd/model/sesion.js
@@ -0,0 +1,12 @@
+export default class Sesion{
+    ci;
+    nombre;
+    idAdmin;
+
+    constructor(ci,nombre,idAdmin){
+        this.ci=ci;
+        this.nombre=nombre;
+        this.idAdmin=idAdmin;
+    }
+
+}
