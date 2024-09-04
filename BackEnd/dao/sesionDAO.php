1119d8f17b72d9c08dabd80cedf2fd8aca84bd15
MateoUYs <mateoindart100@gmail.com>
2024-09-04 17:30:27 -0300

    hskaf

--- BackEnd/dao/sesionDAO.php
+++ BackEnd/dao/sesionDAO.php
@@ -20,7 +20,7 @@
             
             if($fila != null){
                 $respuesta = new Respuesta(true,"sesion iniciada",null);
-                $_SESSION['sesion'] = ["usuario"=>$fila['username'],"email"=>$fila['email'],"isAdmin"=>$fila['isAdmin']];
+                $_SESSION['sesion'] = ["usuario"=>$fila['username'],"email"=>$fila['email'],"isAdmin"=>$fila['isAdmin']==1 ? true:false];
             }else{
                 $respuesta = new Respuesta(false,"Credenciales incorrectas",null);
                 $_SESSION['sesion'] = null;
