6afccf2bc2a5a454328c7671a6e80fd070d7fde0
LautaLope <bas.gdmc@gmail.com>
2024-09-07 19:08:40 -0300

    dsfdsf

--- FrontEnd/page/Usuarios/verificarCuenta/verificarCuenta.js
+++ FrontEnd/page/Usuarios/verificarCuenta/verificarCuenta.js
@@ -1,14 +1,14 @@
 import UserDAO from "../../../dao/userDao.js";
 import SessionDAO from "../../../dao/sesionDAO.js";
 
-let query = await new SessionDAO().getSession();
 
 window.onload = () => {
     mostrarMensaje();
     addEvent();
 }
 
-function mostrarMensaje() {
+async function mostrarMensaje() {
+    let query = await new SessionDAO().getSession();
     let pMensaje = document.querySelector("#descripcion");
     console.log(query.datos);
     pMensaje.innerHTML = `Te hemos enviado un correo con un código de verificación a ${query.datos.email} Por favor, revisa tu bandeja de entrada e ingresa el código en el campo de abajo para continuar.`;
@@ -27,6 +27,7 @@ function addEvent(){
 }
 
 async function verifyUser(code){
+    let query = await new SessionDAO().getSession();
     let verifyQuery = new UserDAO().verifyUser(query.datos.email, code);
     if(verifyQuery.estado){
         console.log("llego");
