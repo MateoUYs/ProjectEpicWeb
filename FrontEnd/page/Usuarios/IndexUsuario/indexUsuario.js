ab79e774549106b4555241ce4b1e8f7cb49b8c6b
Mateo <mateoindart100@gmail.com>
2024-09-30 22:11:14 -0300

    dfgh

--- FrontEnd/page/Usuarios/IndexUsuario/indexUsuario.js
+++ FrontEnd/page/Usuarios/IndexUsuario/indexUsuario.js
@@ -1,5 +1,9 @@
+let menuAbierto = false;
+let cartAbierto = false;
+
 window.onload = () => {
     agregarEventoMenu();
+    agregarEventoCart();
 };
 
 function agregarEventoMenu() {
@@ -7,15 +11,59 @@ function agregarEventoMenu() {
     const panelElemento = document.querySelector("#panelMenu");
 
     menuBoton.onclick = () => {
-        console.log(panelElemento.classList);
-        
+        if (cartAbierto) { 
+            cerrarCarrito();
+        }
 
-        if (panelElemento.classList.contains("panelMenuDesactivado")) {
-            panelElemento.classList.remove("panelMenuDesactivado");
-            panelElemento.classList.add("panelMenuActivado");
+        if (menuAbierto) {
+            cerrarMenu();
         } else {
-            panelElemento.classList.add("panelMenuDesactivado");
-            panelElemento.classList.remove("panelMenuActivado");
+            abrirMenu();
         }
     };
+}
+
+function agregarEventoCart() {
+    const cartBoton = document.querySelector("#cart");
+    const panelElemento = document.querySelector("#panelCart");
+
+    cartBoton.onclick = () => {
+        if (menuAbierto) { 
+            cerrarMenu();
+        }
+
+        if (cartAbierto) {
+            cerrarCarrito();
+        } else {
+            abrirCarrito();
+        }
+    };
+}
+
+function abrirMenu() {
+    const panelElemento = document.querySelector("#panelMenu");
+    panelElemento.classList.remove("panelMenuDesactivado");
+    panelElemento.classList.add("panelMenuActivado");
+    menuAbierto = true;
+}
+
+function cerrarMenu() {
+    const panelElemento = document.querySelector("#panelMenu");
+    panelElemento.classList.add("panelMenuDesactivado");
+    panelElemento.classList.remove("panelMenuActivado");
+    menuAbierto = false;
+}
+
+function abrirCarrito() {
+    const panelElemento = document.querySelector("#panelCart");
+    panelElemento.classList.remove("panelCartDesactivado");
+    panelElemento.classList.add("panelCartActivado");
+    cartAbierto = true;
+}
+
+function cerrarCarrito() {
+    const panelElemento = document.querySelector("#panelCart");
+    panelElemento.classList.add("panelCartDesactivado");
+    panelElemento.classList.remove("panelCartActivado");
+    cartAbierto = false;
 }
