d5ec438a337826774dae78f119efcc73a0557285
Alexis Aquino <alexisfederico@gmail.com>
2024-11-12 19:02:51 -0300

    <sdsadsasd

--- FrontEnd/page/Consultas/gestionarConsultas/gestionarConsultas.js
+++ FrontEnd/page/Consultas/gestionarConsultas/gestionarConsultas.js
@@ -1,9 +1,12 @@
 import SessionDAO from "../../../dao/sessionDAO.js";
 import InquiryDAO from "../../../dao/InquiryDAO.js";
 
+let inquirySelected = null;
+
 window.onload = async () => {
     let queryResponse = await new InquiryDAO().getInquiry();
-    allInquiry = queryResponse.data;
+    console.log(queryResponse);
+    let allInquiry = queryResponse.data;
     let query = await new SessionDAO().getSession();
     if (query.status) {
         if (query.data.isAdmin == 0) {
@@ -13,37 +16,66 @@ window.onload = async () => {
         window.location.href = "../../Usuarios/iniciarSesion/iniciarSesion.html";
     }
     showInquiry(allInquiry);
-    addEvents();
+    addEventModal();
+    
+}
 
-    async function showInquiry(inquirys) {
-        let tbodyElement = document.querySelector("#inquiryData");
-        tbodyElement.innerHTML = "";
-        inquirys.forEach((inquiry) => {
-            let tr = document.createElement("tr");
-            tr.innerHTML += `
-                 <td>${inquiry.inquiryID}</td>
-                 <td>${inquiry.userName}</td>
-                 <td>${inquiry.title}</td>
-                 <td>${inquiry.messageContent}</td>
-            `;
-            let td = document.createElement("td");
-            let div = document.createElement("div");
-            td.appendChild(div);
-            tr.appendChild(td);
-    
-            let btn2 = document.createElement("button");
-            btn2.innerHTML = `<img src="../../../assets/view.png">`;
-            div.appendChild(btn2);
-            btn2.className = "btnTd";
-            btn2.onclick = () => {
-                loadInputs(inquiry);
-                actualUserCi = sale.userCi;
-            }
-    
-            div.id = "actionsTd";
-            tbodyElement.appendChild(tr);
-    
-        });
+
+async function showInquiry(inquirys) {
+    let tbodyElement = document.querySelector("#inquiryData");
+    tbodyElement.innerHTML = "";
+    inquirys.forEach((inquiry) => {
+        let tr = document.createElement("tr");
+        tr.innerHTML += `
+             <td>${inquiry.inquiryId}</td>
+             <td>${inquiry.userName}</td>
+             <td>${inquiry.title}</td>
+        `;
+        let td = document.createElement("td");
+        let div = document.createElement("div");
+        td.appendChild(div);
+        tr.appendChild(td);
+
+        let btn2 = document.createElement("button");
+        btn2.innerHTML = `<img src="../../../assets/view.png">`;
+        div.appendChild(btn2);
+        btn2.className = "btnTd";
+        btn2.onclick = () => {
+            inquirySelected= inquiry;
+           loadMenssage();
+           showModal(true);
+        }
+
+        div.id = "actionsTd";
+        tbodyElement.appendChild(tr);
+
+    });
+}
+
+function loadMenssage(){
+    let title = document.querySelector("#tituloContainer");
+    let message = document.querySelector("#contenidoMensajeContainer");
+    message.innerHTML = "";
+    title.innerHTML = inquirySelected.title;
+    inquirySelected.message.forEach((msj)=>{
+        message.innerHTML += `<p>${msj.content}</p>`;
+    });
+}
+
+function addEventModal(){
+    let modal = document.querySelector("#modal");
+    modal.onclick = (e) => {
+        if (e.target === modal) {
+          
+            showModal(false);
+        }
     }
-    
+}
+
+function showModal(status){
+    if(status){
+        modal.classList.add("show");
+    }else{
+        modal.classList.remove("show");
+    }
 }
