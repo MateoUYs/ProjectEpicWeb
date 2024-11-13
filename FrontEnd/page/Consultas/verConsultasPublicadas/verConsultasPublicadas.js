d33e0db22f6389247d2697bfff03016ef3bde2a8
LautaLope <bas.gdmc@gmail.com>
2024-11-13 17:38:39 -0300

    hjklñ

--- FrontEnd/page/Consultas/verConsultasPublicadas/verConsultasPublicadas.js
+++ FrontEnd/page/Consultas/verConsultasPublicadas/verConsultasPublicadas.js
@@ -1,25 +1,85 @@
-import sessionDAO from '../../../dao/sessionDAO.js';
+import SessionDAO from "../../../dao/sessionDAO.js";
+import InquiryDAO from "../../../dao/InquiryDAO.js";
+
+let inquirySelected = null;
 
 window.onload = async () => {
-    let query = await new sessionDAO().getSession();
-    let registerBtn = document.querySelector("#registerBtn");
-    let logInBtn = document.querySelector("#logInBtn");
-    let userBtn = document.querySelector("#userBtn");
-    let logOutBtn = document.querySelector("#logOutBtn");
-
-    addEvents();
-};
-
-function addEvents() {
-    let userModal = document.querySelector("#userModal");
+    let queryResponse = await new InquiryDAO().getInquiry();
+    console.log(queryResponse);
+    let allInquiry = queryResponse.data;
+    showInquiry(allInquiry);
+    addEventModal();
+   /* getPublicInquiry();*/
 }
 
-userBtn.onclick = () => {
-    if (userModal.classList.contains("modalDisable")) {
-        userModal.classList.add("modalEnable");
-        userModal.classList.remove("modalDisable");
-    } else {
-        userModal.classList.remove("modalEnable");
-        userModal.classList.add("modalDisable");
+
+async function showInquiry(inquirys) {
+    let tbodyElement = document.querySelector("#inquiryData");
+    tbodyElement.innerHTML = "";
+    inquirys.forEach((inquiry) => {
+        let tr = document.createElement("tr");
+        tr.innerHTML += `
+             <td>${inquiry.title}</td>
+        `;
+        let td = document.createElement("td");
+        let div = document.createElement("div");
+        td.appendChild(div);
+        tr.appendChild(td);
+
+        let btn2 = document.createElement("button");
+        btn2.innerHTML = `<img src="../../../assets/view.png">`;
+        div.appendChild(btn2);
+        btn2.className = "btnTd";
+        btn2.onclick = () => {
+            inquirySelected= inquiry;
+           loadMenssage();
+           showModal(true);
+        }
+
+        div.id = "actionsTd";
+        tbodyElement.appendChild(tr);
+
+    });
+}
+
+function loadMenssage(){
+    let title = document.querySelector("#tituloContainer");
+    let message = document.querySelector("#contenidoMensajeContainer");
+    message.innerHTML = "";
+    title.innerHTML = inquirySelected.title;
+    inquirySelected.message.forEach((msj)=>{
+        message.innerHTML += `<p>${msj.content}</p>`;
+    });
+}
+
+/*
+function loadMenssage(){
+    let message = document.querySelector("#contenidoMensajeAdmin")
+    message.innerHTML = "";
+    inquirySelected.message.forEach((msj)=>{
+        message.innerHTML += `<p>Respuesta del administrador: ${msj.content}</p>`;
+    });
+}
+
+async function getPublicInquiry(inquiry){
+    let query = await new InquiryDAO().getPublicInquirys;
+    console.log(query);
+}*/
+
+function addEventModal(){
+    let modal = document.querySelector("#modal");
+    modal.onclick = (e) => {
+        if (e.target === modal) {
+          
+            showModal(false); 
+        }
+    }
+}
+
+function showModal(status){
+    if(status){
+        modal.classList.add("show");
+    }else{
+        modal.classList.remove("show");
     }
 }
